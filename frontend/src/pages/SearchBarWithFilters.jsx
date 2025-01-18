0689cb2c2bdebc003ca723d648898b1a3c4ef115
Albertd13 <albert.ding.13@gmail.com>
2025-01-18 19:37:16 +0800

    Added filter to search bar

--- frontend/src/pages/SearchBarWithFilters.jsx
+++ frontend/src/pages/SearchBarWithFilters.jsx
@@ -1,21 +1,27 @@
 import { Box, Flex, HStack } from "@chakra-ui/react"
 import SearchBar from "./SearchBar"
 import { useState } from "react";
+import FilterSelection from "./FilterSelection";
 
 const SearchBarWithFilters = ({setArticleData}) => {
     const [searchInput, setSearchInput] = useState("");
     const handleInputChange = (event) => {
         setSearchInput(event.target.value); // Update state with user input
     }
-    const filters = {
-        language: 'en',
-
+    const [languageRadioSelection, setLanguageRadioSelection] = useState('English')
+    
+    const language_selections = {
+        English: 'en',
+        French: 'fr',
+        Espanol: "es"
     }
-    const filterString = "&from=2024-12-18&sortBy=relevancy&searchIn=title&language=en"
+    const filterString = 
+        `&from=2024-12-18&sortBy=relevancy&searchIn=title&language=${language_selections[languageRadioSelection]}`
     const url = `https://newsapi.org/v2/everything?q=${searchInput}${filterString}&apiKey=a4ae6e84428346979de39ed52cb40cf6`
     
     const postQueryFilterPred = arti => arti.title !== '[Removed]'
     const handleSearch = () => {
+        console.log(url)
         let fetchRes = fetch(url);
         fetchRes.then(res => res.json())
             .then((data) => {
@@ -34,7 +40,11 @@ const SearchBarWithFilters = ({setArticleData}) => {
                 handleInputChange={ handleInputChange }
                 handleSearch={ handleSearch }
             ></SearchBar>
-            
+            <FilterSelection 
+                selections = {Object.keys(language_selections)} 
+                selected = {languageRadioSelection}
+                setSelectedValue={setLanguageRadioSelection}
+            />
         </HStack>
     )
 }
